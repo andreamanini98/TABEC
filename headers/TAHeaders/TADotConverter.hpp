f674247c73bbbf307990ce76012c19aa4f2ac4be
andreamanini98 <andrea3.manini@mail.polimi.it>
2023-10-20 13:21:28 +0200

    Moved graphviz attributes into separate header file.

--- headers/TAHeaders/TADotConverter.hpp
+++ headers/TAHeaders/TADotConverter.hpp
@@ -6,24 +6,9 @@
 #include "TAHeaders/TAContentExtractor.hpp"
 #include "utilities/JsonHelper.hpp"
 #include "utilities/Utils.hpp"
+#include "defines/GraphvizParameters.h"
 
 using json = nlohmann::json;
-
-// The following defines can be used to tune graphviz parameters
-// -------------------------------------------------------------
-#define FONT_NAME "Helvetica,Arial,sans-serif"
-#define NODESEP "0.3"
-#define NODE_SHAPE "circle"
-#define NODE_WIDTH "0.5"
-#define NODE_HEIGHT "0.5"
-#define INIT_NODE_SHAPE "shape=doublecircle"
-#define FINAL_NODE_STYLE "style=filled, fillcolor=\"#FF00004C\""
-#define NODE_LABEL_COLOR "BLACK"
-#define GUARD_COLOR "GREEN2"
-#define ASSIGNMENT_COLOR "PURPLE"
-#define LOCATION_FONT_SIZE "8pt"
-#define EDGE_FONT_SIZE "8pt"
-#define ARROW_SIZE "0.5"
 
 
 class TADotConverter {
---
+++ headers/defines/GraphvizParameters.h
@@ -0,0 +1,20 @@
+#ifndef UTOTPARSER_GRAPHVIZPARAMETERS_H
+#define UTOTPARSER_GRAPHVIZPARAMETERS_H
+
+// This header contains defines which can be used to tune graphviz parameters.
+
+#define FONT_NAME "Helvetica,Arial,sans-serif"
+#define NODESEP "0.3"
+#define NODE_SHAPE "circle"
+#define NODE_WIDTH "0.5"
+#define NODE_HEIGHT "0.5"
+#define INIT_NODE_SHAPE "shape=doublecircle"
+#define FINAL_NODE_STYLE "style=filled, fillcolor=\"#FF00004C\""
+#define NODE_LABEL_COLOR "BLACK"
+#define GUARD_COLOR "GREEN2"
+#define ASSIGNMENT_COLOR "PURPLE"
+#define LOCATION_FONT_SIZE "8pt"
+#define EDGE_FONT_SIZE "8pt"
+#define ARROW_SIZE "0.5"
+
+#endif //UTOTPARSER_GRAPHVIZPARAMETERS_H
